b5732f5ac63cdcbd93e2131661ead29f3be7dbfd
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2025-08-24 22:31:10 +0300

    Services folder with services logic added

---
+++ Services/AuthService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using security_service.Database.Entities;
+using security_service.Resources.RefreshSessions.Interfaces;
+using security_service.Utils.Classes;
+using System.Security.Cryptography;
+
+namespace security_service.Services
+{
+    public class AuthService
+    {
+        private TokenService _tokenService;
+        private ITempDataDictionaryFactory _tempDataDictionaryFactory;
+        private IHttpClientFactory _httpClientFactory;
+        private IRefreshTokenService _refreshTokenService;
+
+        public AuthService(TokenService tokenService, ITempDataDictionaryFactory tempDataDictionaryFactory, IHttpClientFactory httpClientFactory, IRefreshTokenService refreshTokenService)
+        {
+            _tokenService = tokenService;
+            _tempDataDictionaryFactory = tempDataDictionaryFactory;
+            _httpClientFactory = httpClientFactory;
+            _refreshTokenService = refreshTokenService;
+        }
+
+        public async Task<User> GetUser(CredentialsDTO credentials)
+        {
+            var client = _httpClientFactory.CreateClient();
+            using var response = await client.PostAsJsonAsync("http://users-service:5077/users/login", credentials);
+            User? user = await response.Content.ReadFromJsonAsync<User>();
+
+            return user;
+        }
+
+        public async Task<(string, string)> Login(CredentialsDTO credentials)
+        {     
+            User user = await GetUser(credentials);
+           
+            if (user != null)
+            {
+                string accessToken = _tokenService.GenerateToken(user.Login!, user.Role.ToString());
+                RefreshToken refreshToken = _tokenService.GenerateRefreshToken(user.Id, user.Login, user.Role.ToString());
+
+                _refreshTokenService.Add(refreshToken);
+                return (accessToken, refreshToken.TokenValue!);
+            }
+
+            return ("-1", "-1");
+        }
+        
+        public (string, string) RefreshToken(string token)
+        {
+            var refreshToken = _refreshTokenService.GetTokenByValue(token);                     
+
+            if (refreshToken?.ExpiryDate >  DateTime.Now)
+            {                
+                string newAccessToken = _tokenService.GenerateToken(refreshToken.Username, refreshToken.Role.ToString());
+                return (newAccessToken, refreshToken.TokenValue!);
+            }
+            else
+            {
+                string newAccessToken = _tokenService.GenerateToken(refreshToken.Username, refreshToken.Role.ToString());
+                RefreshToken newRefreshToken = _tokenService.GenerateRefreshToken(refreshToken.UserId, refreshToken.Username, refreshToken.Role);
+
+                refreshToken.TokenValue = newRefreshToken.TokenValue;
+                refreshToken.ExpiryDate = newRefreshToken.ExpiryDate;
+
+                _refreshTokenService.Update(refreshToken);
+                return (newAccessToken, newRefreshToken.TokenValue);
+            }
+        }
+    }
+};
---
+++ Services/TokenService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using security_service.Database.Entities;
+using security_service.Utils.Classes;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace security_service.Services
+{
+    public class TokenService
+    {
+        private const string SecretKey = "super_secret_key_1234567891112131415";
+
+        private const int TokenExpirationMinutes = 60;
+
+        public string GenerateToken(string username, string userRole)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Role, userRole)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "example-app",
+                audience: "example-app-users",
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(TokenExpirationMinutes),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public RefreshToken GenerateRefreshToken(int userId, string username, string role)
+        {
+            var refreshToken = new RefreshToken
+            {
+                UserId = userId,
+                Username = username,
+                Role = role,
+                TokenValue = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+                ExpiryDate = DateTime.UtcNow.AddDays(1)
+            };
+
+            return refreshToken;
+        }
+    }
+}
